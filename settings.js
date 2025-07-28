0263cb123b541f44446b2ae4e3b62c8e80411526
C21MS0N <rendabelrebel2007@gmail.com>
2025-07-28 17:53:25 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "𝙺𝚄𝚁𝙸𝙼𝚄𝚉𝙾𝙽",
+  botOwner: 'ᴄʀɪᴍꜱᴏɴ', // Your name
+  ownerNumber: '2349075268689', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
